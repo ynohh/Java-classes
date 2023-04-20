48222150ba534e21842ed4156f1cbf57818862f4
Yago Leão <yagoleao@mx2.unisc.br>
2023-04-20 20:34:51 -0300

    Add files via upload

---
+++ ex6_1/Funcionarios.java
@@ -0,0 +1,25 @@
+package ex6_1;
+
+public class Funcionarios {
+    private String nome;
+    private float salario;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+
+    
+}
---
+++ ex6_1/Main.java
@@ -0,0 +1,29 @@
+package ex6_1;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        ArrayList<Funcionarios> lista = new ArrayList<Funcionarios>();
+        Scanner sc = new Scanner(System.in);
+        int option;
+
+        System.out.println("1 - Adicionar funcionário");
+        System.out.println("2 - Editar funcionário");
+        System.out.println("3 - Excluir funcionário");
+        System.out.println("4 - Mostrar funcionários");   
+        System.out.println("5 - Sair");   
+        System.out.println("Escolha uma opção: ");   
+        option = sc.nextInt();
+
+        while(option != 5) {
+            
+        }
+
+        for(int i = 0; i < lista.size(); i++) {
+            
+        }
+    }
+
+}
