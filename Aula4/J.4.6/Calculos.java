95fdac8a7d2f8950476f2a20c46caad2ab4de9f2
Yago Leão <yagoleao@mx2.unisc.br>
2023-06-25 19:55:20 -0300

    Add files via upload

---
+++ Aula4/J.4.6/Calculos.java
@@ -0,0 +1,74 @@
+
+public class Calculos {
+    // Métodos para calcular a soma de dois valores
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static double somar(double a, double b) {
+        return a + b;
+    }
+
+    // Métodos para calcular a média de dois valores
+    public static double calcularMedia(int a, int b) {
+        return (a + b) / 2.0;
+    }
+
+    public static double calcularMedia(double a, double b) {
+        return (a + b) / 2.0;
+    }
+
+    // Métodos para calcular a soma de três valores
+    public static int somar(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static double somar(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    // Métodos para calcular a média de três valores
+    public static double calcularMedia(int a, int b, int c) {
+        return (a + b + c) / 3.0;
+    }
+
+    public static double calcularMedia(double a, double b, double c) {
+        return (a + b + c) / 3.0;
+    }
+
+    // Métodos para calcular a soma de quatro valores
+    public static int somar(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    public static double somar(double a, double b, double c, double d) {
+        return a + b + c + d;
+    }
+
+    // Métodos para calcular a média de quatro valores
+    public static double calcularMedia(int a, int b, int c, int d) {
+        return (a + b + c + d) / 4.0;
+    }
+
+    public static double calcularMedia(double a, double b, double c, double d) {
+        return (a + b + c + d) / 4.0;
+    }
+
+    // Métodos para calcular a soma de cinco valores
+    public static int somar(int a, int b, int c, int d, int e) {
+        return a + b + c + d + e;
+    }
+
+    public static double somar(double a, double b, double c, double d, double e) {
+        return a + b + c + d + e;
+    }
+
+    // Métodos para calcular a média de cinco valores
+    public static double calcularMedia(int a, int b, int c, int d, int e) {
+        return (a + b + c + d + e) / 5.0;
+    }
+
+    public static double calcularMedia(double a, double b, double c, double d, double e) {
+        return (a + b + c + d + e) / 5.0;
+    }
+}
